40780a96e8f978fe21f90e6ae3616b76e715d90a
thuraminthein <thur7012@gmail.com>
2025-08-05 13:01:56 +0630

    conf: database config and repository implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"log"
 
+	"github.com/ThuraMinThein/social-golang/internal/db"
 	"github.com/ThuraMinThein/social-golang/internal/env"
+	"github.com/ThuraMinThein/social-golang/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: 	   		env.GetString("DB_ADDR", "postgres://admin:password@localhost/social?sslmode=disable"),
+			maxOpenConns:	env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: 	env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  	env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic("Failed to connect to database: ", err)
+	}
+
+	defer db.Close()
+	log.Println("Connected to database successfully")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mont()
